84ab2a23dc6440977ae5abfdd37062bd1a521816
Jacqueline Mckay H <jacquelinemc@outlook.es>
2023-06-21 18:23:50 -0600

    hotfix: desing fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,9 +22,9 @@ const Contact = () => {
               <p className="contact__data-email">macky_design@some.com</p>
             </div>
             <div className="contact__data-container">
-              <img className="contact__data-linkedin" src="/images/contact__data-linkedin.svg"/>
-              <img className="contact__data-facebook" src="/images/contact__data-facebook.svg"/>
-              <img className="contact__data-twitter" src="/images/contact__data-twitter.svg"/>
+              <a href="https://www.linkedin.com/in/jmckayh/"><img className="contact__data-linkedin" src="/images/contact__data-linkedin.svg" alt="logo linkein"/></a>
+              <img className="contact__data-facebook" src="/images/contact__data-facebook.svg" alt="logo facebook"/>
+              <img className="contact__data-twitter" src="/images/contact__data-twitter.svg" alt="logo twitter"/>
             </div>
           </div>
         </div>
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -14,7 +14,7 @@ const Panel = () => {
           <div className="panel__right">
             <h4 className="panel__title">Pueblo paleta</h4>
             <p className="panel__text">
-              Los Pokémon se presentan en diversas formas y tamaños, y cada uno tiene habilidades y   características únicas. Hay diferentes especies de Pokémon, y se clasifican en diferentes tipos,  como agua, fuego, planta, eléctrico, entre otros. Cada tipo tiene fortalezas y debilidades   específicas en relación con otros tipos.
+              Pueblo Paleta es el lugar de inicio en los juegos de Pokémon en la región de Kanto, donde los jugadores eligen su primer Pokémon y comienzan su aventura. Es conocido por ser el hogar del Profesor Oak y la puerta de entrada al mundo Pokémon.
             </p>
           </div>
         </div>
